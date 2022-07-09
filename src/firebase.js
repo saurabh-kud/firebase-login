c66a94b827697cd6a49255d2a71abc7f8287dc71
saurabh kumar <saurabh322001raj@gmail.com>
2022-07-09 22:53:43 +0530

    .

--- src/firebase.js
+++ src/firebase.js
@@ -2,11 +2,11 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDe4WkGNEf9FAENjytFXSMcrNiZ4PSwk9k",
-  authDomain: "auth-production-87e62.firebaseapp.com",
-  projectId: "auth-production-87e62",
-  storageBucket: "auth-production-87e62.appspot.com",
-  messagingSenderId: "989824657472",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
 };
 
 const app = initializeApp(firebaseConfig);
